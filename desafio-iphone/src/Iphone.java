c9a8189d3d637d59a149a45e313752aaae08d50e
FujiiWill <fujiiwiliam@gmail.com>
2025-01-21 20:07:16 -0300

    Incluão do desafio Iphone

---
+++ desafio-iphone/src/Iphone.java
@@ -0,0 +1,49 @@
+public class Iphone implements ReprodutorMusical, NavegadorInternet, AparelhoTelefonico{
+    public static void main(String[] args) {
+        
+    }
+
+    @Override
+    public void ligar() {
+        
+    }
+
+    @Override
+    public void atender() {
+        
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        
+    }
+
+    @Override
+    public void exibirPagina() {
+        
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        
+    }
+
+    @Override
+    public void atualizarPagina() {
+        
+    }
+    @Override
+    public void tocar() {
+        
+    }
+
+    @Override
+    public void pausar() {
+        
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        
+    }
+}
